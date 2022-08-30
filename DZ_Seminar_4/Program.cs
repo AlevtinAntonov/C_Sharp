c22aaba1a5bbd59d12e94209d34a4d69d52d3ec4
Alevtin_linux_home <antonov3017@gmail.com>
2022-08-30 20:52:04 +0300

    Решение задачи 25

--- DZ_Seminar_4/Program.cs
+++ DZ_Seminar_4/Program.cs
@@ -1,8 +1,20 @@
 ﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
-Console.WriteLine("Задача 25");
 
+Console.WriteLine("Задача 25");
+int numberA = new Random().Next(1,10);
+int numberB = new Random().Next(1,10);
+// int numberA = 3;
+// int numberB = 5;
+
+void Exponentiate (int number, int power)
+{
+    double result = Math.Pow(number, power);
+    Console.WriteLine($"Число А - {number} в степени B - {power} равно {result}"); 
+}
+
+Exponentiate(numberA, numberB);
 
 // Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 
