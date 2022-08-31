747c107fb553c4071244a5e616d3a53a00dfece2
Alevtin_linux_home <antonov3017@gmail.com>
2022-08-31 22:03:09 +0300

    ДЗ Семинар 4 Задачи 25, 27, 29

--- DZ_Seminar_4/Program.cs
+++ DZ_Seminar_4/Program.cs
@@ -15,6 +15,7 @@ void Exponentiate (int number, int power)
 }
 
 Exponentiate(numberA, numberB);
+Console.WriteLine();
 
 // Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 
@@ -24,7 +25,48 @@ Exponentiate(numberA, numberB);
 
 Console.WriteLine("Задача 27");
 
+// Console.WriteLine("Введите число N: "); 
+// int newnumber = Convert.ToInt32(Console.ReadLine());
+int newnumber = new Random().Next(10, 10000);
+void SumOfDigits(int num)
+{
+    int sum = 0;
+    while(num != 0)
+    {
+        sum += num % 10;
+        num /= 10;
+    } 
+    Console.WriteLine($"Сумма цифр числа - {newnumber} равна - {sum}");
+       
+}
+
+SumOfDigits(newnumber);
+Console.WriteLine();
+
 // Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 // 1, 2, 5, 7, 19, 6, 1, 33-> [1, 2, 5, 7, 19, 6, 1, 33]
 
 Console.WriteLine("Задача 29");
+int [] array = new int[8];
+
+void FillArray(int [] collection)
+{
+   for (int index = 0; index < collection.Length; index++)
+   {
+      collection[index] = new Random().Next(0,100);
+   }
+}
+
+void PrintArray(int[] col)
+{
+   // for (int position = 0; position < col.Length; position++)
+   // {
+      // Console.Write(col[position]+ ", ");
+   // }
+   var str = string.Join(", ", col);
+   Console.WriteLine(str);
+   
+}
+
+FillArray(array);
+PrintArray(array);
