5695cd5b9e5ce4eb2ec7aae5af341542642586c7
Alevtin <antonov3017@gmail.com>
2022-09-16 09:55:03 +0300

    Решение задач ДЗ семинар 9 : 64, 66, 68

---
+++ DZ_Seminar_9/Task_66/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите число M: "); 
+int M = Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine("Введите число N: "); 
+int N = Convert.ToInt32(Console.ReadLine()); 
+
+int SumOfNaturalNumbersInRange (int M,int N) 
+{ 
+    if(M<=N) return (M + SumOfNaturalNumbersInRange(M+1,N));
+    else return 0; 
+} 
+
+if (M <N) Console.WriteLine($"Сумма натуральных элементов в промежутке от {M} до {N} -> {SumOfNaturalNumbersInRange(M,N)}");
+else Console.WriteLine($"Ошибка! Второе число M: {M} больше первого числа N:  {N}");
